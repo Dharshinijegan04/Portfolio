4bb9d204db3767b359281ff904909d59faa225f2
Dharshini jegan <dharshini.jegan04@gmail.com>
2026-03-05 20:54:31 +0530

    Update Footer.js

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -4,20 +4,15 @@ import "./Footer.css";
 function Footer() {
   const currentYear = new Date().getFullYear();
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
-
   return (
     <footer className="footer">
-   
-        <div className="copyright">
-          <p>
-            © {currentYear} Selva Dharshini J. Made with <span className="heart">❤️</span> All rights reserved.
-          </p>
-        </div>
+      <div className="copyright">
+        <p>
+          © {currentYear} Selva Dharshini J. Made with <span className="heart">❤️</span> All rights reserved.
+        </p>
+      </div>
     </footer>
   );
-}    
+}
 
 export default Footer;
